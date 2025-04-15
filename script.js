0016f5c1fcae76f8ec4a5030e57272ffba4fa563
Rafcksdev <rafcksps@gmail.com>
2025-04-14 21:46:26 -0300

    feat: Insert image

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-$(document).ready(function(){
-    $("header button").click( () => {
-        alert("Button clicked!");
+$(document).ready(function () {
+    $("header button").click(() => {
+        $("form").slideDown();
     });
-    
+
+    $(".cancelar").click(() => {
+        $("form").slideUp();
+    });
+
     $("form").on("submit", (event) => {
         event.preventDefault();
+
+        const address = $("input").val().trim();
+
+        const newLi = $("<li></li>");
+        const newImage = $("<img>").attr("src", address);
+
+        newLi.append(newImage);
+        $("ul").append(newLi);
+        $("input").val("");
+        $("form").slideUp();
     });
-})
+});
